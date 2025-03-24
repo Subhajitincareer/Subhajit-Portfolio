5e30bee2c2d096ca469008c999b67a84322ea34b
w3schools-test <subhajitincareer@gmail.com>
2025-03-24 20:26:35 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
-  basePath: '/Personal-Portfolio/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'], // Separate React dependencies
-          'three-vendor': ['three'], // Separate Three.js
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Increase the limit to avoid warnings
-  }
-}
+  basePath: '/Personal-Portfolio/',}
 )
